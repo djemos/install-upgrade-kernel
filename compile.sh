d7f69a9ac79d17b79f199e0c2f93f69881b736a6
djemos <dijemos@gmail.com>
2019-09-23 22:01:34 +0300

    update

--- compile.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-for i in `ls po/*.po`;do
-	echo "Compiling `echo $i|sed "s|po/||"`"
-	msgfmt $i -o `echo $i |sed "s/.po//"`.mo
-done
-
-intltool-merge po/ -d -u install-upgrade-kernel.desktop.in install-upgrade-kernel.desktop
