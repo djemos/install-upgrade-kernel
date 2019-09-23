937b15ca42fa0cd8c3ae3d7438534f589445925e
djemos <dijemos@gmail.com>
2019-09-23 21:42:51 +0300

    add files

---
+++ install.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+install -d -m 755 $DESTDIR/usr/bin/
+install -d -m 755 $DESTDIR/usr/share/pixmaps
+install -m 755 src/install-upgrade-kernel $DESTDIR/usr/bin/
+install -m 644 src/install-upgrade-kernel.png $DESTDIR/usr/share/pixmaps/
+
+for i in `ls po/*.mo|sed "s|po/\(.*\).mo|\1|"`; do
+	install -d -m 755 $DESTDIR/usr/share/locale/${i}/LC_MESSAGES
+	install -m 644 po/${i}.mo \
+	$DESTDIR/usr/share/locale/${i}/LC_MESSAGES/install-upgrade-kernel.mo
+done
